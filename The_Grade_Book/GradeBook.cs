ecd674005d9190929e2803cf20f2027cdd962876
Jan <jan.steyaert@vdabcampus.be>
2018-12-20 15:25:48 +0100

    gradebook pluralsight v1

---
+++ The_Grade_Book/GradeBook.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Grade_Book
+{
+    class GradeBook
+    {
+
+        public GradeBook()
+        {
+            grades = new List<float>();
+        }
+        public GradeStatistics ComputeStatistics()
+        {
+            GradeStatistics stats = new GradeStatistics();
+            
+            float sum = 0;
+            foreach(float grade in grades)
+            {
+                stats.HighGrade = Math.Max(grade, stats.HighGrade);
+                stats.LowGrade = Math.Min(grade, stats.LowGrade);
+                sum += grade;
+            }
+            if (grades.Count != 0)
+            {
+                stats.AvgGrade = sum / grades.Count;
+            }
+            return stats;
+        }
+
+
+
+        public void AddGrade(float grade)
+        {
+            grades.Add(grade);
+        }
+        List<float> grades;
+    }
+}
---
+++ The_Grade_Book/GradeStatistics.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Grade_Book
+{
+    class GradeStatistics
+    {
+        public GradeStatistics()
+        {
+            HighGrade = 0;
+            LowGrade = float.MaxValue;
+        }
+        public float AvgGrade;
+        public float HighGrade;
+        public float LowGrade;
+    }
+}
